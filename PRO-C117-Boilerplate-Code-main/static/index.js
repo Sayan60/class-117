3559d88dc9e2ed696d2c807cf71ab982a9fefd06
Sayan60 <72643308+Sayan60@users.noreply.github.com>
2022-05-27 18:59:15 +0530

    Add files via upload

---
+++ PRO-C117-Boilerplate-Code-main/static/index.js
@@ -0,0 +1,53 @@
+//Create date variable
+var date = new Date()
+let display_date = "date:" + date.toLocaleDateString()
+
+//Load HTML DOM
+$(document).ready(function(){
+    $("#display_date").html(display_date)
+    
+    
+})
+//Define variable to store predicted emotion
+
+
+//HTML-->JavaScript--->Flask
+//Flask--->JavaScript--->HTML
+
+//jQuery selector and click action
+
+$(function () {
+    $("#predict_button").click(function () {
+        //AJAX call
+        let input_data = {
+            "text":$("#text").val()
+        }
+        console.log(input_data)
+        $.ajax({
+            type : 'POST',
+            url:"/predict-emotion",
+            data:JSON.stringify(input_data),
+            dataType:"json",
+            contentType:'application/json',
+            succes:function(result)
+            
+              {
+                predicted_emotion = result.data.predicted_emotion
+                emo_url = result.data.predicted_emotion_img_url
+                // Result Received From Flask ----->JavaScript
+                $("#prediction").html(predicted_emotion) 
+                $('#prediction').css("display", "block");
+                 $("#emo_img_url").attr('src', emo_url);
+                  $('#emo_img_url').css("display", "block");
+
+                // Display Result Using JavaScript----->HTML
+                
+            },
+            //Error function
+            error: function (result) { 
+                alert(result.responseJSON.message) 
+            }
+        });
+    });
+})
+
